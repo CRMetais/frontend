cf2a61d837fee170959fb401810ecd0108da8f48
GuilhermeEduardoGS <guilherme.gsouza@sptech.school>
2026-02-23 10:18:27 -0300

    Integração tela Resumo v1

--- front-end-cr-metais/src/components/Resumo.jsx
+++ front-end-cr-metais/src/components/Resumo.jsx
@@ -1,77 +1,75 @@
 import "../styles/Resumo.css";
+import { useState, useEffect } from "react";
+import { api } from "../services/resumoService";
 
-const produtos = [
-  { id: 1, nome: "Aerosol", peso: "123kg", valorKg: "R$10,00", total: "R$1.230,00" },
-  { id: 2, nome: "Aerosol", peso: "123kg", valorKg: "R$10,00", total: "R$1.230,00" },
-  { id: 3, nome: "Aerosol", peso: "123kg", valorKg: "R$10,00", total: "R$1.230,00" },
-  { id: 4, nome: "Aerosol", peso: "123kg", valorKg: "R$10,00", total: "R$1.230,00" },
-  { id: 5, nome: "Aerosol", peso: "123kg", valorKg: "R$10,00", total: "R$1.230,00" },
-  { id: 6, nome: "Aerosol", peso: "123kg", valorKg: "R$10,00", total: "R$1.230,00" },
-  { id: 7, nome: "Aerosol", peso: "123kg", valorKg: "R$10,00", total: "R$1.230,00" },
-  { id: 8, nome: "Aerosol", peso: "123kg", valorKg: "R$10,00", total: "R$1.230,00" },
-  { id: 9, nome: "Aerosol", peso: "123kg", valorKg: "R$10,00", total: "R$1.230,00" },
-  { id: 10, nome: "Aerosol", peso: "123kg", valorKg: "R$10,00", total: "R$1.230,00" },
-  { id: 11, nome: "Aerosol", peso: "123kg", valorKg: "R$10,00", total: "R$1.230,00" },
-];
-
-const EstoqueHeader = () =>{
-    return (
-        <div className="estoqueHeader">
-            <span className="estoque-produto">Produto</span>
-            <span className="estoque-peso">Peso</span>
-            <span className="estoque-valor">Valor</span>
-            <span className="estoque-total">Total</span>
-        </div>
-    );
+const EstoqueHeader = () => {
+  return (
+    <div className="estoque-header">
+      <span className="estoque-produto">Produto</span>
+      <span className="estoque-peso">Peso</span>
+      <span className="estoque-valor">Valor</span>
+      <span className="estoque-total">Total</span>
+    </div>
+  );
 };
 
-const EstoqueItem = ({ produtos }) => {
-    return (
-        <div className="estoque-line">
-            <div className="estoque-item">
-                <span className="estoque-produto">{produtos.nome}</span>
-                <span className="estoque-peso">{produtos.peso}</span>
-                <span className="estoque-valor">{produtos.valorKg}</span>
-                <span className="estoque-total">{produtos.total}</span>
-            </div>
-            <div className="divisao"></div>
-        </div>
-    );
+const EstoqueItem = ({ produto }) => {
+  return (
+    <div className="estoque-line">
+      <div className="estoque-item">
+        <span className="estoque-produto">{produto.nome}</span>
+        <span className="estoque-peso">{produto.peso || "N/A"}</span>
+        <span className="estoque-valor">{produto.valorKg || "N/A"}</span>
+        <span className="estoque-total">{produto.total || "N/A"}</span>
+      </div>
+      <div className="divisao"></div>
+    </div>
+  );
 };
 
-const ResumoCard = ({ titulo, valor}) => {
-    return (
-        <div className="resumo-card">
-            <span className="resumo-titulo">{titulo}</span>
-            <h3>{valor}</h3>
-        </div>
-    );
+const ResumoCard = ({ titulo, valor }) => {
+  return (
+    <div className="resumo-card">
+      <span className="resumo-titulo">{titulo}</span>
+      <h3>{valor}</h3>
+    </div>
+  );
 };
 
-const Resumo = ({ lista = produtos }) => {
-    return (
-        <div className="estoque-container">
+const Resumo = () => {
+  const [produtos, setProdutos] = useState([]);
 
-            <div className="estoque-esq">
-                <h1 className="estoque-titulo">Estoque Atual</h1>
-               
-                <div className="estoque-grid">
-                <EstoqueHeader />
-                {lista.map((produtos) => (
-                    <EstoqueItem key={produtos.id} produtos={produtos} />
-                ))}
-                </div>
-            </div>
+  useEffect(() => {
+    api.get('/produtos')
+      .then(res => {
+        console.log('Resposta da API:', res.data);
+        setProdutos(res.data);
+      })
+      .catch(err => console.error('Erro ao buscar produtos:', err));
+  }, []);
 
-            <div className="estoque-dir">
-                <ResumoCard titulo="Total Aplicado:" valor="R$ 43.231,67" />
-                <ResumoCard titulo="Peso Total:" valor="7.545,500 kg" />
-                <ResumoCard titulo="Pg Notas (hoje):" valor="R$ 12.458,32" />
-                <ResumoCard titulo="Peso Kg (hoje):" valor="1.145,200 kg" />
-            </div>
-
+  return (
+    <div className="estoque-container">
+      <div className="estoque-esq">
+        <h1 className="estoque-titulo">Estoque Atual</h1>
+        <div className="estoque-grid">
+          <EstoqueHeader />
+          <div className="estoque-lista">
+          {produtos.map(produto => (
+            <EstoqueItem key={produto.idProduto} produto={produto} />
+          ))}
+          </div>
         </div>
-    );
+      </div>
+
+      <div className="estoque-dir">
+        <ResumoCard titulo="Total Aplicado:" valor="R$ 43.231,67" />
+        <ResumoCard titulo="Peso Total:" valor="7.545,500 kg" />
+        <ResumoCard titulo="Pg Notas (hoje):" valor="R$ 12.458,32" />
+        <ResumoCard titulo="Peso Kg (hoje):" valor="1.145,200 kg" />
+      </div>
+    </div>
+  );
 };
 
 export default Resumo;
