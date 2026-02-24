f0062081d1d508061df0150b7ad0d1ff15ffe8ac
MiguelCSerpa <miguel.serpa@sptech.school>
2026-02-24 20:04:48 -0300

    teste

--- front-end-cr-metais/src/components/Navbar.jsx
+++ front-end-cr-metais/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import "../styles/NavbarStyle.css";
+import styles from "../styles/NavbarStyle.module.css";
 import logo from "../styles/img/LOGO.png";
 
 export default function Navbar({ currentPage, setCurrentPage }) {
@@ -15,29 +15,33 @@ export default function Navbar({ currentPage, setCurrentPage }) {
   const handleItemClick = (item) => {
     setCurrentPage(item);
   };
-  
+
   const handleLogout = () => {
     setCurrentPage("Login");
   };
-  
+
   return (
-    <nav className="navbar">
-      <div className="logo">
-        <img src={logo} alt="Logo" id="logo"/>
-        <p className="texto_logo">CR Metais</p>
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>
+        <img src={logo} alt="Logo" className={styles.logoImg} />
+        <p className={styles.texto_logo}>CR Metais</p>
       </div>
-      <ul className="item_list">
-        {items.map(item => (
-          <li 
-            key={item} 
-            className={currentPage === item ? "active" : "inactivated"}
+
+      <ul className={styles.item_list}>
+        {items.map((item) => (
+          <li
+            key={item}
+            className={currentPage === item ? styles.active : ""}
             onClick={() => handleItemClick(item)}
           >
             {item}
           </li>
         ))}
       </ul>
-      <button className="btn_sair" onClick={handleLogout}>Sair</button>
+
+      <button className={styles.btn_sair} onClick={handleLogout}>
+        Sair
+      </button>
     </nav>
   );
 }
