f4e14150f446c62365c7c51d8be9eb818b78954a
GuilhermeEduardoGS <guilherme.gsouza@sptech.school>
2026-03-26 14:21:42 -0300

    Modularizando o Resumo.css

--- front-end-cr-metais/src/components/Resumo.jsx
+++ front-end-cr-metais/src/components/Resumo.jsx
@@ -1,37 +1,48 @@
-import "../styles/Resumo.css";
+import styles from "../styles/Resumo.module.css";
 import { useState, useEffect } from "react";
 import { api } from "../services/resumoService";
 
 const EstoqueHeader = () => {
   return (
-    <div className="estoque-header">
-      <span className="estoque-produto">Produto</span>
-      <span className="estoque-peso">Peso (Kg)</span>
-      <span className="estoque-valor">Valor Unitário (R$)</span>
-      <span className="estoque-total">Total (R$)</span>
+    <div className={styles.estoqueHeader}>
+      <span className={styles.estoqueProduto}>Produto</span>
+      <span className={styles.estoquePeso}>Peso (Kg)</span>
+      <span className={styles.estoqueValor}>Valor Unitário (R$)</span>
+      <span className={styles.estoqueTotal}>Total (R$)</span>
     </div>
   );
 };
 
-
 const EstoqueItem = ({ produto }) => {
   return (
-    <div className="estoque-line">
-      <div className="estoque-item">
-        <span className="estoque-produto">{produto.nome}</span>
-        <span className="estoque-peso">{produto.peso.toFixed(2)}</span>
-        <span className="estoque-valor">{produto.valor.toFixed(2)}</span>
-        <span className="estoque-total">{produto.total.toFixed(2)}</span>
+    <div className={styles.estoqueLine}>
+      <div className={styles.estoqueItem}>
+        <span className={styles.estoqueProduto}>{produto.nome}</span>
+        <span className={styles.estoquePeso}>
+          {`${produto.peso.toLocaleString("pt-BR")} Kg`}
+        </span>
+        <span className={styles.estoqueValor}>
+          {produto.valor.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          })}
+        </span>
+        <span className={styles.estoqueTotal}>
+          {produto.total.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          })}
+        </span>
       </div>
-      <div className="divisao"></div>
+      <div className={styles.divisao}></div>
     </div>
   );
 };
 
 const ResumoCard = ({ titulo, valor }) => {
   return (
-    <div className="resumo-card">
-      <span className="resumo-titulo">{titulo}</span>
+    <div className={styles.resumoCard}>
+      <span className={styles.resumoTitulo}>{titulo}</span>
       <h3>{valor}</h3>
     </div>
   );
@@ -40,47 +51,33 @@ const ResumoCard = ({ titulo, valor }) => {
 const Resumo = () => {
   const [resumo, setResumo] = useState(null);
 
-  // Quando for editar sem usar o java e o bd descomenta esse useEffect mockado e comenta o outro
-
-  //   useEffect(() => {
-  //   setResumo({
-  //     produtos: [
-  //       { nome: "Produto A", peso: 10, valor: 20, total: 200 },
-  //       { nome: "Produto B", peso: 5, valor: 30, total: 150 }
-  //     ],
-  //     totalAplicado: 350,
-  //     pesoTotal: 15,
-  //     notasHoje: 2,
-  //     pesoHoje: 15
-  //   });
-  // }, []);
-
-
-  // Usa esse useEffect quando ligar o java e bd
-
-      useEffect(() => {
-        document.title = "CR Metais | Resumo"
-    })
   useEffect(() => {
-    api.get('/resumos')
-      .then(res => {
-        console.log('Resposta da API:', res.data);
+    document.title = "CR Metais | Resumo";
+  }, []);
+
+  useEffect(() => {
+    api
+      .get("/resumos")
+      .then((res) => {
+        console.log("Resposta da API:", res.data);
         setResumo(res.data);
       })
-      .catch(err => console.error('Erro ao buscar produtos:', err));
+      .catch((err) => console.error("Erro ao buscar produtos:", err));
   }, []);
 
   if (!resumo) {
-    return <p>Carregando...</p>
+    return <p>Carregando...</p>;
   }
 
   return (
-    <div className="estoque-container">
-      <div className="estoque-esq">
-        <h1 className="estoque-titulo">Estoque Atual</h1>
-        <div className="estoque-grid">
+    <div className={styles.estoqueContainer}>
+      <div className={styles.estoqueEsq}>
+        <h1 className={styles.estoqueTitulo}>Estoque Atual</h1>
+
+        <div className={styles.estoqueGrid}>
           <EstoqueHeader />
-          <div className="estoque-lista">
+
+          <div className={styles.estoqueLista}>
             {resumo.produtos.map((produto, index) => (
               <EstoqueItem key={index} produto={produto} />
             ))}
@@ -88,12 +85,30 @@ const Resumo = () => {
         </div>
       </div>
 
-      <div className="estoque-dir">
-        <div className="container-cards">
-          <ResumoCard titulo="Total Aplicado:" valor={`R$ ${resumo.totalAplicado.toFixed(2)}`} />
-          <ResumoCard titulo="Peso Total:" valor={`${resumo.pesoTotal.toFixed(2)} Kg`} />
-          <ResumoCard titulo="Pg Notas (hoje):" valor={`${resumo.notasHoje.toFixed(2)} Kg`} />
-          <ResumoCard titulo="Peso Kg (hoje):" valor={`${resumo.pesoHoje.toFixed(2)}`} />
+      <div className={styles.estoqueDir}>
+        <div className={styles.containerCards}>
+          <ResumoCard
+            titulo="Total Aplicado:"
+            valor={resumo.totalAplicado.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          />
+          <ResumoCard
+            titulo="Peso Total:"
+            valor={`${resumo.pesoTotal.toLocaleString("pt-BR")} Kg`}
+          />
+          <ResumoCard
+            titulo="Pg Notas (hoje):"
+            valor={resumo.notasHoje.toLocaleString("pt-BR", {
+              style: "currency",
+              currency: "BRL",
+            })}
+          />
+          <ResumoCard
+            titulo="Peso Kg (hoje):"
+            valor={`${resumo.pesoHoje.toLocaleString("pt-BR")} Kg`}
+          />
         </div>
       </div>
     </div>
